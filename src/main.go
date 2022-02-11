45540cb9849f48356d641d1cb3e89da296fcd9dd
unknown <julien.castillejos@epitech.eu>
2022-02-11 15:24:23 +0100

    Add go.mod

--- src/main.go
+++ src/main.go
@@ -1,4 +1,19 @@
 package main
 
 func main() {
+	width := 5
+	height := 5
+	length := width * height
+	nodes := make([]Node, length)
+
+	warehouse_graph := WarehouseSquareGraph{
+		nodes:  nodes,
+		width:  width,
+		height: height,
+	}
+	warehouse_graph.initializeNodes()
+	warehouse_graph.addPackage("box_1", 1, 1, GREEN)
+	warehouse_graph.addTransporter("transporter_1", 2, 0)
+	warehouse_graph.addTruck("truck_1", 3, 4, 10, 50)
+	warehouse_graph.print()
 }
