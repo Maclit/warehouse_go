6915eb840ebd163122175fb39fe1644041d28635
unknown <julien.castillejos@epitech.eu>
2022-02-11 16:09:23 +0100

    Remove useless coordinates

--- warehouse_graph.go
+++ warehouse_graph.go
@@ -1,6 +1,6 @@
 package main
 
-import . "fmt"
+import "fmt"
 
 const (
 	GREEN  int = 0
@@ -11,19 +11,13 @@ const (
 type Box struct {
 	name  string
 	color int
-	x     int
-	y     int
 }
 type Transporter struct {
 	name string
-	x    int
-	y    int
 }
 
 type Truck struct {
 	name         string
-	x            int
-	y            int
 	max_load     int
 	waiting_time int
 }
@@ -50,39 +44,33 @@ func (graph *WarehouseSquareGraph) initializeNodes() {
 
 func (graph *WarehouseSquareGraph) addPackage(name string, x, y, color int) {
 	graph.nodes[x+(y*graph.height)].box.name = name
-	graph.nodes[x+(y*graph.height)].box.x = x
-	graph.nodes[x+(y*graph.height)].box.y = y
 	graph.nodes[x+(y*graph.height)].box.color = color
 }
 
 func (graph *WarehouseSquareGraph) addTruck(name string, x, y, max_load, waiting_time int) {
 	graph.nodes[x+(y*graph.height)].truck.name = name
-	graph.nodes[x+(y*graph.height)].truck.x = x
-	graph.nodes[x+(y*graph.height)].truck.y = y
 	graph.nodes[x+(y*graph.height)].truck.max_load = max_load
 	graph.nodes[x+(y*graph.height)].truck.waiting_time = waiting_time
 }
 
 func (graph *WarehouseSquareGraph) addTransporter(name string, x, y int) {
 	graph.nodes[x+(y*graph.height)].transporter.name = name
-	graph.nodes[x+(y*graph.height)].transporter.x = x
-	graph.nodes[x+(y*graph.height)].transporter.y = y
 }
 
 func (graph *WarehouseSquareGraph) print() {
 	for i := 0; i < graph.width*graph.height; i++ {
 		if i != 0 && i%graph.height == 0 {
-			Print("\n")
+			fmt.Print("\n")
 		}
 		node := graph.nodes[i]
 		if node.box.name != "" {
-			Print("[x]")
+			fmt.Print("[x]")
 		} else if node.transporter.name != "" {
-			Print("[t]")
+			fmt.Print("[t]")
 		} else if node.truck.name != "" {
-			Print("[g]")
+			fmt.Print("[g]")
 		} else {
-			Print("[ ]")
+			fmt.Print("[ ]")
 		}
 	}
 }
