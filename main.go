6f067a7dc0cd92f42bf77d5ab874d5789101ce44
NGUYEN HUYNH <corentin.nguyen-huynh@epitech.eu>
2022-02-19 10:23:22 +0100

    add parsing method

--- main.go
+++ main.go
@@ -1,13 +1,50 @@
 package main
 
 func main() {
-	width := 5
-	height := 5
+	//width := 5
+	//height := 5
 
-	warehouse_graph := WarehouseSquareGraph{width: width, height: height}
+	text1 := "5 5 1000"
+	text2 := "box_1 1 1 green"
+	text3 := "transporter_1 2 0"
+	text4 := "truck 3 4 10 50"
+
+	words_1, err := divide(text1)
+	if err != nil {
+		return
+	}
+	words_2, err := divide(text2)
+	if err != nil {
+		return
+	}
+	words_3, err := divide(text3)
+	if err != nil {
+		return
+	}
+	words_4, err := divide(text4)
+	if err != nil {
+		return
+	}
+	size, err := info_map(words_1)
+	if err != nil {
+		return
+	}
+	warehouse_graph := WarehouseSquareGraph{width: size[0], height: size[1]}
 	warehouse_graph.initializeNodes()
-	warehouse_graph.addPackage("box_1", 1, 1, GREEN)
+	err2 := find_object(words_2, &warehouse_graph)
+	if err2 != nil {
+		return
+	}
+	err3 := find_object(words_3, &warehouse_graph)
+	if err3 != nil {
+		return
+	}
+	err4 := find_object(words_4, &warehouse_graph)
+	if err4 != nil {
+		return
+	}
+	/*warehouse_graph.addPackage("box_1", 1, 1, GREEN)
 	warehouse_graph.addTransporter("transporter_1", 2, 0)
-	warehouse_graph.addTruck("truck_1", 3, 4, 10, 50)
+	warehouse_graph.addTruck("truck_1", 3, 4, 10, 50)*/
 	warehouse_graph.print()
 }
---
+++ parsing.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type ErrInput []string
+
+func (e ErrInput) Error() string {
+	return fmt.Sprintf("can not accept more than 5 parameters and less than 3. number of parameters: %d", len(e))
+}
+
+type ErrMap string
+
+func (e ErrMap) Error() string {
+	return fmt.Sprintf("the input for the map is not a number or not greater than zero, string: %s ", e)
+}
+
+type ErrColor string
+
+func (e ErrColor) Error() string {
+	return fmt.Sprintf("this input does not correspond to an color accepted by the program, string: %s ", e)
+}
+
+func info_map(text []string) ([]int, error) {
+	infoMap := []int{}
+
+	for n := 0; n < len(text); n++ {
+		intVar, err := strconv.Atoi(text[n])
+		if err != nil || intVar < 0 {
+			MapNull := []int{}
+			return MapNull, ErrMap(text[n])
+		}
+		infoMap = append(infoMap, intVar)
+	}
+	return infoMap, nil
+}
+
+func info_colis(text []string) (string, []int, error) {
+	infoColis := []int{}
+	infoNameColis := text[0]
+
+	for n := 1; n < len(text)-1; n++ {
+		intVar, err := strconv.Atoi(text[n])
+		if err != nil || intVar < 0 {
+			ColisPosNull := []int{}
+			ColisNull := ""
+			return ColisNull, ColisPosNull, ErrMap(text[n])
+		}
+		infoColis = append(infoColis, intVar)
+	}
+	switch lower := strings.ToLower(text[len(text)-1]); lower {
+	case "green":
+		infoColis = append(infoColis, GREEN)
+	case "yellow":
+		infoColis = append(infoColis, YELLOW)
+	case "blue":
+		infoColis = append(infoColis, BLUE)
+	default:
+		ColisPosNull := []int{}
+		ColisNull := ""
+		return ColisNull, ColisPosNull, ErrColor(lower)
+	}
+	return infoNameColis, infoColis, nil
+}
+
+func info_palette_camion(text []string) (string, []int, error) {
+	infoPosColis := []int{}
+	infoColis := text[0]
+
+	for n := 1; n < len(text); n++ {
+		intVar, err := strconv.Atoi(text[n])
+		if err != nil || intVar < 0 {
+			ColisPosNull := []int{}
+			ColisNull := ""
+			return ColisNull, ColisPosNull, ErrMap(text[n])
+		}
+		infoPosColis = append(infoPosColis, intVar)
+	}
+	return infoColis, infoPosColis, nil
+}
+
+func find_object(words []string, warehouse *WarehouseSquareGraph) error {
+	switch size := len(words); size {
+	case 4:
+		name, pos_color, err := info_colis(words)
+		if err != nil {
+			return err
+		}
+		warehouse.addPackage(name, pos_color[0], pos_color[1], pos_color[2])
+	case 3:
+		name, pos, err := info_palette_camion(words)
+		if err != nil {
+			return err
+		}
+		warehouse.addTransporter(name, pos[0], pos[1])
+	case 5:
+		name, pos_size_round, err := info_palette_camion(words)
+		if err != nil {
+			return err
+		}
+		warehouse.addTruck(name, pos_size_round[0], pos_size_round[1], pos_size_round[2], pos_size_round[3])
+	}
+	return nil
+}
+
+func divide(text string) ([]string, error) {
+	words := strings.Fields(text)
+	if len(words) < 3 || len(words) > 5 {
+		response := []string{}
+		return response, ErrInput(words)
+	}
+	return words, nil
+}
