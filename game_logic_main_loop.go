d125b5d65a195a8de4c13c49139bbb4bfce1bbd9
unknown <julien.castillejos@epitech.eu>
2022-02-24 09:11:26 +0100

    Separate game logic

---
+++ game_logic_main_loop.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isGameFinished(graph *WarehouseSquareGraph) bool {
+	for i := 0; i < graph.width*graph.height; i++ {
+		node := graph.nodes[i]
+		if node.box != nil {
+			return false
+		}
+		if node.transporter != nil && node.transporter.isLoaded {
+			return false
+		}
+	}
+	return true
+}
+
+func play(turns int, graph *WarehouseSquareGraph) {
+	for i := 0; i < turns; i++ {
+		fmt.Printf("tour %d\n", i+1)
+		transporterNodeList := graph.getWharehouseNodeListWithObject(TRANSPORTER)
+		for _, transporterNode := range transporterNodeList {
+			if transporterNode.transporter.isLoaded {
+				moveTransporterTowardNearestTruck(graph, transporterNode)
+			} else {
+				moveTransporterTowardNearestBox(graph, transporterNode)
+			}
+		}
+		truckNodeList := graph.getWharehouseNodeListWithObject(TRUCK)
+		for _, truckNode := range truckNodeList {
+			updateTruckStatus(graph, truckNode)
+		}
+		if isGameFinished(graph) && graph.areAllTrucksGone() {
+			emoji, err := strconv.Unquote(`"😎"`)
+			if err == nil {
+				fmt.Println("\n", emoji)
+			}
+			return
+		}
+		fmt.Println()
+	}
+	emoji, err := strconv.Unquote(`"🙂"`)
+	if err == nil {
+		fmt.Println("\n", emoji)
+	}
+}
--- warehouse_graph_algorithm.go
+++ warehouse_graph_algorithm.go
@@ -1,63 +1,5 @@
 package main
 
-import "fmt"
-
-func (graph *WarehouseSquareGraph) moveTransporterTowardNearestBox(startNode Node) {
-	fmt.Print(startNode.transporter.name)
-	if graph.isEmpty() {
-		if graph.doesNodeHasObject(startNode.point, TRUCK) {
-			neighbor, err := getEmptyNeighbor(graph, startNode)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-			graph.moveTransporterToNextPosition(startNode.point, neighbor.point)
-		} else {
-			fmt.Print(" WAIT\n")
-		}
-		return
-	}
-	closestBox := graph.findClosestObject(startNode, BOX)
-	if closestBox.box == nil {
-		return
-	}
-	shortestPath := graph.shortestPath(startNode, closestBox, make([]Node, 0))
-	if len(shortestPath) == 2 {
-		boxX := shortestPath[1].point.x
-		boxY := shortestPath[1].point.y
-		transporterX := startNode.point.x
-		transporterY := startNode.point.y
-		graph.nodes[transporterX+(transporterY*graph.height)].transporter.isLoaded = true
-		graph.nodes[transporterX+(transporterY*graph.height)].transporter.box = graph.nodes[boxX+(boxY*graph.height)].box
-		fmt.Printf(" TAKE %s ", graph.nodes[boxX+(boxY*graph.height)].box.name)
-		switch graph.nodes[boxX+(boxY*graph.height)].box.color {
-		case YELLOW:
-			fmt.Print("YELLOW\n")
-		case GREEN:
-			fmt.Print("GREEN\n")
-		case BLUE:
-			fmt.Print("BLUE\n")
-		}
-		graph.nodes[boxX+(boxY*graph.height)].box = nil
-	} else {
-		graph.moveTransporterToNextPosition(startNode.point, shortestPath[1].point)
-	}
-}
-
-func (graph *WarehouseSquareGraph) moveTransporterTowardNearestTruck(startNode Node) {
-	fmt.Print(startNode.transporter.name)
-	if graph.doesNodeHasObject(startNode.point, TRUCK) {
-		graph.unloadTransporter(startNode.point)
-	} else {
-		closestBox := graph.findClosestObject(startNode, TRUCK)
-		if closestBox.truck == nil {
-			fmt.Print(" WAIT\n")
-			return
-		}
-		shortestPath := graph.shortestPath(startNode, closestBox, make([]Node, 0))
-		graph.moveTransporterToNextPosition(startNode.point, shortestPath[1].point)
-	}
-}
-
 func (graph *WarehouseSquareGraph) shortestPath(start Node, end Node, path []Node) []Node {
 	path = append(path, start)
 	if graph.isNodeInArray(path, end) {
@@ -96,29 +38,4 @@ func (graph *WarehouseSquareGraph) findClosestObject(startNode Node, toFind int)
 		}
 	}
 	return Node{}
-}
-
-func (graph *WarehouseSquareGraph) updateTruckStatus(truckNode Node) {
-	fmt.Print(truckNode.truck.name)
-	x := truckNode.point.x
-	y := truckNode.point.y
-	if graph.nodes[x+(y*graph.height)].truck.currentTimer == graph.nodes[x+(y*graph.height)].truck.maxTimer {
-		graph.nodes[x+(y*graph.height)].truck.isGone = true
-	}
-	if graph.nodes[x+(y*graph.height)].truck.isGone {
-		graph.nodes[x+(y*graph.height)].truck.currentTimer--
-		fmt.Printf(" GONE %d/%d\n", graph.nodes[x+(y*graph.height)].truck.currentLoad, graph.nodes[x+(y*graph.height)].truck.maxLoad)
-		if graph.nodes[x+(y*graph.height)].truck.currentTimer == 0 {
-			graph.nodes[x+(y*graph.height)].truck.isGone = false
-			graph.nodes[x+(y*graph.height)].truck.currentLoad = 0
-		}
-		return
-	}
-	if graph.isGameFinished() {
-		graph.nodes[x+(y*graph.height)].truck.currentTimer = graph.nodes[x+(y*graph.height)].truck.maxTimer
-	}
-	fmt.Printf(" WAITING %d/%d\n", graph.nodes[x+(y*graph.height)].truck.currentLoad, graph.nodes[x+(y*graph.height)].truck.maxLoad)
-	if graph.nodes[x+(y*graph.height)].truck.currentLoad == graph.nodes[x+(y*graph.height)].truck.maxLoad {
-		graph.nodes[x+(y*graph.height)].truck.currentTimer = graph.nodes[x+(y*graph.height)].truck.maxTimer
-	}
 }
