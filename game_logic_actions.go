609fb4f14e07216031df688680dbb9160909fb6e
unknown <julien.castillejos@epitech.eu>
2022-02-24 09:18:55 +0100

    Move logic

--- game_logic_actions.go
+++ game_logic_actions.go
@@ -57,3 +57,28 @@ func moveTransporterTowardNearestTruck(graph *WarehouseSquareGraph, startNode No
 		graph.moveTransporterToNextPosition(startNode.point, shortestPath[1].point)
 	}
 }
+
+func updateTruckStatus(graph *WarehouseSquareGraph, truckNode Node) {
+	fmt.Print(truckNode.truck.name)
+	x := truckNode.point.x
+	y := truckNode.point.y
+	if graph.nodes[x+(y*graph.height)].truck.currentTimer == graph.nodes[x+(y*graph.height)].truck.maxTimer {
+		graph.nodes[x+(y*graph.height)].truck.isGone = true
+	}
+	if graph.nodes[x+(y*graph.height)].truck.isGone {
+		graph.nodes[x+(y*graph.height)].truck.currentTimer--
+		fmt.Printf(" GONE %d/%d\n", graph.nodes[x+(y*graph.height)].truck.currentLoad, graph.nodes[x+(y*graph.height)].truck.maxLoad)
+		if graph.nodes[x+(y*graph.height)].truck.currentTimer == 0 {
+			graph.nodes[x+(y*graph.height)].truck.isGone = false
+			graph.nodes[x+(y*graph.height)].truck.currentLoad = 0
+		}
+		return
+	}
+	if isGameFinished(graph) {
+		graph.nodes[x+(y*graph.height)].truck.currentTimer = graph.nodes[x+(y*graph.height)].truck.maxTimer
+	}
+	fmt.Printf(" WAITING %d/%d\n", graph.nodes[x+(y*graph.height)].truck.currentLoad, graph.nodes[x+(y*graph.height)].truck.maxLoad)
+	if graph.nodes[x+(y*graph.height)].truck.currentLoad == graph.nodes[x+(y*graph.height)].truck.maxLoad {
+		graph.nodes[x+(y*graph.height)].truck.currentTimer = graph.nodes[x+(y*graph.height)].truck.maxTimer
+	}
+}
