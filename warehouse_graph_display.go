6b366bff1f71c0ded71bc4d8a4bae8498dd7ebe9
unknown <julien.castillejos@epitech.eu>
2022-02-24 09:36:12 +0100

    lint

--- warehouse_graph_display.go
+++ warehouse_graph_display.go
@@ -2,8 +2,7 @@ package main
 
 import "fmt"
 
-// Print A method to print the graph on the terminal.
-func (graph *WarehouseSquareGraph) Print() {
+func (graph *WarehouseSquareGraph) print() {
 	for i := 0; i < graph.width*graph.height; i++ {
 		if i != 0 && i%graph.height == 0 {
 			fmt.Print("\n")
